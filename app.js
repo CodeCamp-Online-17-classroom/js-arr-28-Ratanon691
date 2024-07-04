19d605518c01d9a80b3b08e2a603da06905690d1
Non T <117605943+Ratanon691@users.noreply.github.com>
2024-07-04 10:36:28 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-// write code here
+function deleteTask(id){
+  const tasks = [
+  { id: 1, name: 'Fishing' },
+  { id: 2, name: 'Shopping' },
+  { id: 3, name: 'Swimming' }
+];
+  for(i = 0 ; i < tasks.length ; i++){
+    if(tasks[i].id == id){
+    tasks.splice(i, 1)
+    }
+  }
+  console.log(tasks)
+}
+deleteTask(2);
+
+// [
+//   { id: 1, name: 'Fishing' },
+//   { id: 3, name: 'Swimming' }
+// ]
